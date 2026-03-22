18325a2850f6cdf5909f6be03430f497eaf022e9
Lucas Tinoco <lucas.tinoco@hotmail.com>
2026-03-22 00:16:10 -0300

    feat: add Lava fluid type

--- Aero_FluidType.java
+++ Aero_FluidType.java
@@ -4,11 +4,13 @@ public class Aero_FluidType {
 	public static final int NONE = 0;
 	public static final int WATER = 1;
 	public static final int HEAVY_WATER = 2;
+	public static final int LAVA = 3;
 
 	public static String getName(int type) {
 		switch (type) {
 			case WATER: return "Water";
 			case HEAVY_WATER: return "Heavy Water";
+			case LAVA: return "Lava";
 			default: return "None";
 		}
 	}
@@ -17,6 +19,7 @@ public class Aero_FluidType {
 		switch (type) {
 			case WATER: return 0xFF3344FF;
 			case HEAVY_WATER: return 0xFF1A237E;
+			case LAVA: return 0xFFFF6600;
 			default: return 0xFFFFFFFF;
 		}
 	}
